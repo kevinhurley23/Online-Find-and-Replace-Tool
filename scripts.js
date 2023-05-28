ec8165259d0c3d776c5e70f9194b0a9ad89fcf9f
Kevin Hurley <kevinhurley23@gmail.com>
2023-05-28 15:39:45 -0400

    functionally complete

--- scripts.js
+++ scripts.js
@@ -1,36 +1,41 @@
 $(document).ready(function () {
+  const initialRows = $("#rows-container").html();
+
   function addRow() {
-    let row = $(this).closest(".row");
+    let row = $(this).closest(".outer-row");
     let newRow = row.clone();
+    newRow
+      .find(".mode input")
+      .attr("name", "mode-" + ($("#rows-container .outer-row").length + 1));
     newRow.find('input[type="text"]').val("");
+    newRow.find('.mode input[value="basic"]').prop("checked", true);
     newRow.insertAfter(row);
+    updateLabels();
   }
 
   function deleteRow() {
-    let row = $(this).closest(".row");
-    let numRows = $("#rows-container .row").length;
+    let row = $(this).closest(".outer-row");
+    let numRows = $("#rows-container .outer-row").length;
     if (numRows > 1) {
       row.remove();
     }
+    updateLabels();
   }
 
   function updateLabels() {
-    $("#rows-container .row").each(function (index) {
+    $("#rows-container .outer-row").each(function (index) {
       $(this)
         .find(".find-label")
         .text("Find #" + (index + 1));
       $(this)
         .find(".replace-label")
         .text("Replace #" + (index + 1));
-      $(this)
-        .find(".mode input")
-        .attr("name", "mode-" + (index + 1));
     });
   }
 
   function replaceText() {
     let inputText = $("#input-text").val();
-    $("#rows-container .row").each(function () {
+    $("#rows-container .outer-row").each(function () {
       let findText = $(this).find(".find").val();
       let replaceText = $(this).find(".replace").val();
       let mode = $(this).find(".mode input:checked").val();
@@ -51,9 +56,72 @@ $(document).ready(function () {
     return string.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
   }
 
+  function reset() {
+    $("#rows-container").html(initialRows);
+  }
+
+  function saveSearchPattern() {
+    let rows = $("#rows-container .outer-row").map(function () {
+      let findValue = $(this).find(".find").val();
+      let replaceValue = $(this).find(".replace").val();
+      let modeValue = $(this).find(".mode input:checked").val();
+      return { find: findValue, replace: replaceValue, mode: modeValue };
+    });
+    console.log(rows);
+    let currentDate = new Date().toISOString();
+    let patternData = {
+      type: "searchPattern",
+      date: currentDate,
+      rows: rows.toArray(),
+    };
+    $("#save-modal").show();
+    $("#save-button").on("click", function () {
+      let patternName = $("#pattern-name").val();
+      if (patternName) {
+        localStorage.setItem(patternName, JSON.stringify(patternData));
+      }
+      $("#pattern-name").val("");
+      $("#save-modal").hide();
+    });
+  }
+
+  function loadSearchPattern() {
+    $("#pattern-list").empty();
+    let searchPatterns = Object.keys(localStorage).filter((key) => {
+      let item = JSON.parse(localStorage.getItem(key));
+      return item.type === "searchPattern";
+    });
+    $("#load-modal").show();
+    searchPatterns.forEach((pattern) => {
+      let item = JSON.parse(localStorage.getItem(pattern));
+      let date = new Date(item.date).toLocaleString();
+      let button = $(`<button>${pattern} (${date})</button>`);
+      button.on("click", function () {
+        $("#rows-container").html("");
+        item.rows.forEach((row, index) => {
+          let newRow = $(initialRows);
+          newRow.find(".find").val(row.find);
+          newRow.find(".replace").val(row.replace);
+          newRow.find(`.mode input`).attr("name", `mode-${index + 1}`);
+          newRow.find(`.mode input[value="${row.mode}"]`).prop("checked", true);
+          $("#rows-container").append(newRow);
+        });
+        updateLabels();
+        $("#load-modal").hide();
+      });
+      $("#pattern-list").append(button);
+    });
+  }
+
   $("#rows-container").on("click", ".add-row", addRow);
-  $("#rows-container").on("click", ".add-row", updateLabels);
   $("#rows-container").on("click", ".delete-row", deleteRow);
-  $("#rows-container").on("click", ".delete-row", updateLabels);
   $("#replace-all").on("click", replaceText);
+  $("#reset").on("click", reset);
+  $("#save").on("click", saveSearchPattern);
+  $("#load").on("click", loadSearchPattern);
+  $(".modal").on("click", function (event) {
+    if (event.target === this) {
+      $(".modal").hide();
+    }
+  });
 });
